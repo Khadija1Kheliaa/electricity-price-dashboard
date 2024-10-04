9826b546ec38066136c94578d916f282165ba2da
Ahmed Khliaa <ahmed.khliaa@p3-goup.com>
2024-10-04 11:53:32 +0200

    add the files of the projects with description

---
+++ data_fetch.py
@@ -0,0 +1,4 @@
+"""
+This file contains the logic for making API requests to fetch the latest electricity prices. 
+It dynamically calculates the last 24 hours and formats the data for use in the dashboard.
+"""
---
+++ data_processing.py
@@ -0,0 +1,3 @@
+"""
+(Optional) This file contains data processing functions such as cleaning, normalizing, or further transforming the fetched electricity price data before it is visualized.
+"""
